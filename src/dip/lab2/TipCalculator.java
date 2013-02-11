98ca8684f2979d952e1fa7217f3a509fcae0faf3
tim78317 <tliebl@my.wctc.edu>
2013-02-11 00:14:00 -0600

    really having trouble with this

---
+++ src/dip/lab2/TipCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2;
+
+/**
+ *
+ * @author tim78317
+ */
+public interface TipCalculator {
+
+    double getTip();
+    
+    
+}
---
+++ src/dip/lab2/TipService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dip.lab2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tim78317
+ */
+public class TipService {
+    
+    private TipCalculator tipCalculator; 
+
+    public TipService(TipCalculator tipCalculator) {
+        this.tipCalculator = tipCalculator;
+    }
+    
+    
+    
+    
+    public void getTip(TipCalculator tipCalculator){
+      
+         
+    }
+    
+}
